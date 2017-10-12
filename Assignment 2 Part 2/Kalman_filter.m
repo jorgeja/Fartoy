af921639e2fb72a4378170ed79a072d91a7c6f9c
jorgeja <mrjackwitz@gmail.com>
2017-10-12 09:41:47 +0200

    endret navn på simulinkdiagrammet

--- Assignment 2 Part 2/Kalman_filter.m
+++ Assignment 2 Part 2/Kalman_filter.m
@@ -1,6 +1,6 @@
 function [ x_hat_dot, P_dot ] = Kalman_filter( x_hat, u, y, P )
-K = P*C'\R;
+K = P*C'*inv(R);
 x_hat_dot = A*x_hat + B*u + K*(y - C*x_hat);
-P_dot = A*P + P*A' + E*Q*E' - P*C'\R*C*P;
+P_dot = A*P + P*A' + E*Q*E' - P*C'*inv(R)*C*P;
 end
 
